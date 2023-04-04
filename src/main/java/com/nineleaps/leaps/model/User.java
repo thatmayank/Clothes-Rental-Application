8aaf9e25f3c8af62a522a273fcadacf2daeb8c5a
Mayank <thatmayank@gmail.com>
2023-04-04 05:39:17 +0530

    Migrating code from previous repository to this one

---
+++ src/main/java/com/nineleaps/leaps/model/User.java
@@ -0,0 +1,35 @@
+package com.nineleaps.leaps.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private @NotBlank String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    private @NotBlank String email;
+    @Column(name = "phone_number")
+    private @NotBlank String phoneNumber;
+    private @NotBlank String password;
+
+    public User(String firstName, String lastName, String email, String phoneNumber, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+}
