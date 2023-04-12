097d781213eca01e9cd0aec0223eab16cf4f10a0
Mayank <thatmayank@gmail.com>
2023-04-12 12:46:03 +0530

    Added wishlist category and all the code till now

---
+++ src/main/java/com/nineleaps/leaps/controller/CartController.java
@@ -0,0 +1,13 @@
+package com.nineleaps.leaps.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+    //Add to cart
+    //Get products of cart
+    //update the cart
+    //remove from cart
+}
--- src/main/java/com/nineleaps/leaps/controller/ProductController.java
+++ src/main/java/com/nineleaps/leaps/controller/ProductController.java
@@ -6,6 +6,7 @@ import com.nineleaps.leaps.model.Product;
 import com.nineleaps.leaps.model.categories.Category;
 import com.nineleaps.leaps.model.categories.SubCategory;
 import com.nineleaps.leaps.service.*;
+import com.nineleaps.leaps.utils.Helper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,31 +20,41 @@ import java.util.*;
 public class ProductController {
     private final ProductServiceInterface productService;
     private final SubCategoryServiceInterface subCategoryService;
+    private final CategoryServiceInterface categoryService;
 
     @Autowired
-    public ProductController(ProductServiceInterface productService, SubCategoryServiceInterface subCategoryService) {
+    public ProductController(ProductServiceInterface productService, SubCategoryServiceInterface subCategoryService, CategoryServiceInterface categoryService) {
         this.productService = productService;
         this.subCategoryService = subCategoryService;
+        this.categoryService = categoryService;
     }
 
     @PostMapping("/add")
-    public ResponseEntity<ApiResponse> addProduct(@RequestBody ProductDto productDto) {
+    public ResponseEntity<ApiResponse> addProduct(@RequestBody @Valid ProductDto productDto) {
+        List<Category> categories = new ArrayList<>();
+        for (Long categoryId : productDto.getCategoryIds()) {
+            Optional<Category> optionalCategory = categoryService.readCategory(categoryId);
+            if (!optionalCategory.isPresent()) {
+                return new ResponseEntity<>(new ApiResponse(false, "Category is invalid"), HttpStatus.NOT_FOUND);
+            }
+            categories.add(optionalCategory.get());
+        }
         List<SubCategory> subCategories = new ArrayList<>();
         for (Long subcategoryId : productDto.getSubcategoryIds()) {
             Optional<SubCategory> optionalSubCategory = subCategoryService.readSubCategory(subcategoryId);
             if (!optionalSubCategory.isPresent()) {
-                return new ResponseEntity<ApiResponse>(new ApiResponse(false, "category is invalid"), HttpStatus.CONFLICT);
+                return new ResponseEntity<>(new ApiResponse(false, "Subcategory is invalid"), HttpStatus.CONFLICT);
             }
             subCategories.add(optionalSubCategory.get());
         }
-        productService.addProduct(productDto, subCategories);
-        return new ResponseEntity<ApiResponse>(new ApiResponse(true, "Product has been added"), HttpStatus.CREATED);
+        productService.addProduct(productDto, subCategories, categories);
+        return new ResponseEntity<>(new ApiResponse(true, "Product has been added"), HttpStatus.CREATED);
     }
 
     @GetMapping("/list")
     public ResponseEntity<List<ProductDto>> listProducts() {
         List<ProductDto> body = productService.listProducts();
-        return new ResponseEntity<List<ProductDto>>(body, HttpStatus.OK);
+        return new ResponseEntity<>(body, HttpStatus.OK);
     }
 
     @PutMapping("/update/{productId}")
@@ -52,16 +63,25 @@ public class ProductController {
         if (!optionalProduct.isPresent()) {
             return new ResponseEntity<>(new ApiResponse(false, "Product is invalid"), HttpStatus.NOT_FOUND);
         }
+        List<Category> categories = new ArrayList<>();
+        for (Long categoryId : productDto.getCategoryIds()) {
+            Optional<Category> optionalCategory = categoryService.readCategory(categoryId);
+            if (!optionalCategory.isPresent()) {
+                return new ResponseEntity<>(new ApiResponse(false, "Category is invalid"), HttpStatus.NOT_FOUND);
+            }
+            categories.add(optionalCategory.get());
+        }
         List<SubCategory> subCategories = new ArrayList<>();
         for (Long subcategoryId : productDto.getSubcategoryIds()) {
             Optional<SubCategory> optionalSubCategory = subCategoryService.readSubCategory(subcategoryId);
             if (!optionalSubCategory.isPresent()) {
-                return new ResponseEntity<ApiResponse>(new ApiResponse(false, "category is invalid"), HttpStatus.CONFLICT);
+                return new ResponseEntity<>(new ApiResponse(false, "Subcategory is invalid"), HttpStatus.CONFLICT);
             }
             subCategories.add(optionalSubCategory.get());
         }
-        productService.updateProduct(productId, productDto, subCategories);
-        return new ResponseEntity<ApiResponse>(new ApiResponse(true, "Product has been updated"), HttpStatus.OK);
+
+        productService.updateProduct(productId, productDto, subCategories, categories);
+        return new ResponseEntity<>(new ApiResponse(true, "Product has been updated"), HttpStatus.OK);
     }
 
     //list by subcategory id
@@ -72,10 +92,33 @@ public class ProductController {
         if (!optionalSubCategory.isPresent()) {
             return new ResponseEntity<>(new ArrayList<>(), HttpStatus.NOT_FOUND);
         }
-        //fetch the subcategories accordingly
+        //fetch the products accordingly
         List<Product> body = productService.listProductsById(subcategoryId);
         return new ResponseEntity<>(body, HttpStatus.OK);
     }
 
+    //list by category id
+    @PutMapping("/listbycategoryId/{categoryId}")
+    public ResponseEntity<List<ProductDto>> listByCategoryId(@PathVariable("categoryId") Long categoryId) {
+        //check if category id is valid
+        Optional<Category> optionalCategory = categoryService.readCategory(categoryId);
+        if (!optionalCategory.isPresent()) {
+            return new ResponseEntity<>(new ArrayList<>(), HttpStatus.NOT_FOUND);
+        }
+        // fetch the products accordingly
+        List<ProductDto> body = productService.listProductsByCategoryId(categoryId);
+        return new ResponseEntity<>(body, HttpStatus.OK);
+    }
 
+    //list by product id
+    @PutMapping("/listByProductId/{productId}")
+    public ResponseEntity<ProductDto> listByProductId(@PathVariable("productId") Long productId) {
+        //check if product id is valid
+        ProductDto product = productService.listProductByid(productId);
+        if (!Helper.notNull(product)) {
+            return new ResponseEntity<>(new ProductDto(), HttpStatus.NOT_FOUND);
+        }
+        //fetch the product details
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
 }
